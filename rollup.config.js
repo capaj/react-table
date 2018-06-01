7ccd0e85bedbdb0057e5c76e3636e251378cf3ac
Jiri Spac <capajj@gmail.com>
2018-06-01 11:48:08 +0200

    build changed and namespaced

--- rollup.config.js
+++ rollup.config.js
@@ -2,41 +2,49 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 import commonjs from 'rollup-plugin-commonjs'
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
+import autoExternal from 'rollup-plugin-auto-external'
 
 const env = process.env.NODE_ENV
 
 const config = {
   input: 'src/index.js',
-  output: {
-    file: env === 'production' ? 'react-table.min.js' : 'react-table.js',
-    format: 'umd',
-    globals: {
-      react: 'React',
-    },
-    name: 'ReactTable',
-    exports: 'named',
-  },
   external: ['react'],
   plugins: [
-    nodeResolve(),
-    babel({
-      exclude: '**/node_modules/**',
+    commonjs({
+      include: ['node_modules/classnames/index.js', 'node_modules/prop-types/index.js'],
     }),
+    nodeResolve(),
+    babel(),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env),
     }),
-    commonjs(),
+
+    autoExternal(),
+  ],
+  output: [
+    {
+      format: 'umd',
+      file: env === 'production' ? 'react-table.min.js' : 'react-table.js',
+      globals: {
+        react: 'React',
+      },
+      name: 'ReactTable',
+      exports: 'named',
+    },
+    { file: 'dist/react-table.es.js', format: 'es' },
   ],
 }
 
 if (env === 'production') {
-  config.plugins.push(uglify({
-    compress: {
-      dead_code: true,
-      warnings: false,
-    },
-  }))
+  config.plugins.push(
+    uglify({
+      compress: {
+        dead_code: true,
+        warnings: false,
+      },
+    })
+  )
 }
 
 export default config
